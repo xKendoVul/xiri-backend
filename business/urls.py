6f8a0543421daf7fe4d5856ab5d013f273673d49
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-07-20 09:15:24 -0600

    Register BusinessQualification model in the router

--- business/urls.py
+++ business/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BusinessViewSet, MenuViewSet, FoodCollectionViewSet, RouteBusinessViewSet
+from .views import BusinessViewSet, MenuViewSet, FoodCollectionViewSet, RouteBusinessViewSet, BusinessQualificationViewSet
 
 router = DefaultRouter()
 router.register(r'business', BusinessViewSet)
 router.register(r'menus', MenuViewSet)
 router.register(r'collections', FoodCollectionViewSet)
 router.register(r'route-business', RouteBusinessViewSet)
+router.register(r'qualifications', BusinessQualificationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
