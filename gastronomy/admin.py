f6382649a112309c9626c5404a08744bc60044a5
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-07-19 23:40:15 -0600

    Register GastronomicRoute and RouteBusiness models in admin interface

--- gastronomy/admin.py
+++ gastronomy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Department, Food
+from .models import Department, Food, GastronomicRoute, RouteBusiness
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
@@ -12,3 +12,15 @@ class FoodAdmin(admin.ModelAdmin):
 
     list_filter = ('department_origin',)
     search_fields = ('name', 'description')
+
+@admin.register(GastronomicRoute)
+class GastronomicRouteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+    list_filter = ('department',)
+    search_fields = ('name',)
+
+@admin.register(RouteBusiness)
+class RouteBusinessAdmin(admin.ModelAdmin):
+    list_display = ('route', 'business', 'suggested_order')
+    list_filter = ('route',)
+    ordering = ('route', 'suggested_order')
