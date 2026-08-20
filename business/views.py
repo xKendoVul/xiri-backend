04575370750864383c425f0dc526436f702fe6a4
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-08-19 23:11:29 -0600

    Add status import to views.py for improved response handling

--- business/views.py
+++ business/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import PermissionDenied
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from django.db import models
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
